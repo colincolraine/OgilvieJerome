28d88d7d7404fa0a4a3ca224a3f78e00667e90c9
colin colraine <colinco83@hotmail.co.uk>
2021-12-04 18:04:49 +0000

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
-const PORT =(process.env.PORT) ? process.env.PORT : 5000;
+const PORT =(process.env.PORT) ? process.env.PORT : 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.json())
